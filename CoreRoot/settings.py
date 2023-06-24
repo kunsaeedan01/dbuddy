27b0e31802ff0eef0567f3bca59add4ba7e0cb86
Lenovo LEGION <rakhat.erezhepov02@gmail.com>
2023-06-24 17:30:35 +0600

    updated allowed hosts

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-f%##rse6qch66c7z*ob9vz8rn_muf1y!s4ennqjs)4d7wkwl=d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
