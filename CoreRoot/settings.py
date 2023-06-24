ebf2d707f9449ccbfe62e7275b60cf4d731a0318
Lenovo LEGION <rakhat.erezhepov02@gmail.com>
2023-06-24 17:06:58 +0600

    db owner changed

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'coredb',
-        'USER': 'postgres',
+        'USER': 'core',
         'PASSWORD': '0512rahat',
         'HOST': 'localhost',
         'PORT': '5432',
